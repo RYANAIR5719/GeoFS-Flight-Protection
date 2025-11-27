cdba98c8c16c027c50f80d9f1c9e50e4aaf6c353
RYANAIR5719 <127127127+RYANAIR5719@users.noreply.github.com>
2025-11-26 19:58:27 -0500

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GeoFS Flight Protection
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Asks for confirmation before processing Tab or v keys in GeoFS to prevent accidental reset.
 // @author       RYANAIR5719
 // @match        https://www.geo-fs.com/geofs.php*
@@ -11,35 +11,37 @@
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    window.addEventListener('keydown', function(e) {
+    window.addEventListener('keydown', function (e) {
 
-        if (!e.ctrlKey && !e.metaKey && !e.altKey) {
+        var target = e.target || e.srcElement;
+        if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.isContentEditable) {
+            return;
+        }
 
-            if (e.code === 'Tab') {
-                let userConfirmed = confirm(`Are you sure you want to teleport to the camera location?`);
-                if (!userConfirmed) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    e.stopImmediatePropagation();
-                    console.log(`Blocked ${e.code} press.`);
-                } else {
-                    console.log(`Confirmed ${e.code} press.`);
-                }
+        if (e.code === 'Tab') {
+            let userConfirmed = confirm(`Are you sure you want to teleport to the camera location?`);
+            if (!userConfirmed) {
+                e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+                console.log(`Blocked ${e.code} press.`);
+            } else {
+                console.log(`Confirmed ${e.code} press.`);
             }
+        }
 
-            else if (e.code === 'KeyV') {
-                let userConfirmed = confirm(`Are you sure you want to open replay mode?`);
-                if (!userConfirmed) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    e.stopImmediatePropagation();
-                    console.log(`Blocked ${e.code} press.`);
-                } else {
-                    console.log(`Confirmed ${e.code} press.`);
-                }
+        else if (e.code === 'KeyV') {
+            let userConfirmed = confirm(`Are you sure you want to open replay mode?`);
+            if (!userConfirmed) {
+                e.preventDefault();
+                e.stopPropagation();
+                e.stopImmediatePropagation();
+                console.log(`Blocked ${e.code} press.`);
+            } else {
+                console.log(`Confirmed ${e.code} press.`);
             }
         }
     }, true);
