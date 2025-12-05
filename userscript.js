a358576cf748460df0206a2e3ae5c7a63edfbb89
RYANAIR5719 <127127127+RYANAIR5719@users.noreply.github.com>
2025-12-05 09:57:04 -0500

    Update version and icon in userscript.js

--- userscript.js
+++ userscript.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         GeoFS Flight Protection
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Asks for confirmation for critical keybinds to prevent accidental flight interference.
 // @author       RYANAIR5719
 // @match        https://www.geo-fs.com/geofs.php*
 // @match        https://geo-fs.com/geofs.php*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=geo-fs.com
+// @icon         https://raw.githubusercontent.com/RYANAIR5719/GeoFS-Flight-Protection/refs/heads/main/logo.png
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
